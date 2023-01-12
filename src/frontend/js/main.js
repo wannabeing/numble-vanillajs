44b4332d3489731ddbf7da40c3a7a55956b16b86
wannabeing <wannabeing@naver.com>
2023-01-12 11:19:16 +0900

    ADD upBtn

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -3,3 +3,31 @@ import "../scss/styles.scss"; // scss
 // [Lozad.js]: lazy loading 구현
 const observer = lozad();
 observer.observe();
+
+const upBtn = document.querySelector(".upBtn"); // 화면 위로 올라가는 스크롤 버튼
+let isScrolling; // 스크롤 감지 변수
+
+// 🚀 업 버튼 클릭 시
+upBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  // 화면 상단으로 이동
+  window.scroll({
+    behavior: "smooth",
+    top: 0,
+  });
+});
+
+// scroll 감지
+addEventListener("scroll", (event) => {
+  event.stopPropagation();
+
+  window.clearTimeout(isScrolling);
+  upBtn.classList.remove("upBtnVisible");
+
+  // Set a timeout to run after scrolling ends
+  isScrolling = setTimeout(function () {
+    // Run the callback
+    console.log("Scrolling has stopped.");
+    upBtn.classList.add("upBtnVisible");
+  }, 100);
+});
